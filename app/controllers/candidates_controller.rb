5f72c8671d69668c1d4c6f7d9635ad5e8fcab1bd
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-11-10 16:25:00 +0800

    創建 new 表單，使用form_helper, debugger , flash 功能 創建 index 頁面，顯示創建的資料

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,6 +1,26 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
   end
   def new
+    @candidate = Candidate.new
   end
+  def create
+    @candidate = Candidate.new(candidate_params)
+    if @candidate.save
+      flash[:notice] = "Candidate created successfully!!!"
+      redirect_to candidates_path
+    else
+      error = @candidate.errors.full_messages
+      flash.now[:alert] = error
+      render :new
+    end
+  end
+
+  private
+
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
 end

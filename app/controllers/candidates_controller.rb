d926bafc7df12687e78080d07266bc899ec008c1
Zhe Kuan Lin <53500272+VincentLin0619@users.noreply.github.com>
2023-11-14 11:40:14 +0800

    新增 CadidateController(:edit, :update) 抽離錯誤訊息為模板 抽離表單為模板 CandidateController 裡的成功跳轉與錯誤處理獨立成 private method(:save_and_redirect, :errors)

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,8 @@
 class CandidatesController < ApplicationController
+
+  before_action :find_params_id, only: [:show, :edit, :update]
+  # before_action :errors, only: [:create, :update]
+
   def index
     @candidates = Candidate.all
   end
@@ -9,24 +13,43 @@ class CandidatesController < ApplicationController
 
   def create
     @candidate = Candidate.new(candidate_params)
-    if @candidate.save
-      flash[:notice] = "Candidate created successfully!!!"
-      redirect_to candidates_path
-    else
-      error = @candidate.errors.full_messages
-      flash.now[:alert] = error
-      render :new
-    end
+    save_and_redirect(@candidate, candidates_path, "Candidate created successfully!!!", :new)
   end
 
   def show
-    @candidate = Candidate.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    save_and_redirect(@candidate, candidates_path, "Candidate updated successfully!!!", :edit)
+  end
+
 
   private
 
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
+  def find_params_id
+    @candidate = Candidate.find(params[:id])
+  end
+
+  # 成功跳轉與錯誤訊息
+  def save_and_redirect(obj, path, msg, render)
+    if obj.save
+      redirect_to(path)
+      flash[:notice] = msg
+    else
+      render(render)
+      flash.now[:alert] = errors(obj)
+    end
+  end
+
+  def errors(obj)
+    obj.errors.full_messages if obj.errors.any?
   end
 
 end
